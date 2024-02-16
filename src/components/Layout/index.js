282ed13ce0b8969e51421993813afb38f4626973
freddychenyouren2 <freddychenyouren2@gmail.com>
2024-02-16 14:02:26 +0800

    Set up portfolio basic styling and Sidebar navigation
    Basic font and images set up, with CSS and colour scheme. No content has been added yet.

---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import './index.scss';
+import Sidebar from '../Sidebar'
+
+//Functional components
+
+const Layout = () => {
+    return (
+        <div className='App'>
+          <Sidebar/>
+        </div>
+    );
+}
+
+export default Layout;
