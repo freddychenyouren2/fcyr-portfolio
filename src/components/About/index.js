521f3081fb82ff30de533a968d40c8914f179201
freddychenyouren2 <freddychenyouren2@gmail.com>
2024-02-16 18:33:38 +0800

    Add rotating cube showcasing skills and pacman loading icon in About

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,14 +1,30 @@
 import AnimatedLetters from '../AnimatedLetters'
+import { useState, useEffect } from 'react';
 import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCss3, faCss3Alt, faGitAlt, faGithub, faGithubAlt, faHtml5, faJava, faJsSquare, faPython, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
 
 const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate');
     const aboutMe = "About Me"
     const aboutMeArray = Array.from({length: aboutMe.length}, (_, i) => aboutMe[i])
+
+    useEffect(() => {
+        setTimeout(() => {
+          return setLetterClass('text-animate-hover')
+        }, 2500)
+      }, [])
+
     return (
+        <>
+        
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
                     <AnimatedLetters
+                    letterClass={letterClass}
                         stringArray={aboutMeArray}
                         index={15}
                     />
@@ -35,7 +51,40 @@ const About = () => {
                 </p>
                 
             </div>
+
+            <div className='stage-cube-cont'>
+                <div className='cubeSpinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faHtml5} color='#F06529'/>
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faCss3Alt} color='#28A4D9'/>
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faReact} color='#5ED4F4'/>
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faJsSquare} color='#EFD81D'/>
+                    </div>
+                    <div className='face5'>
+                        <FontAwesomeIcon icon={faPython} color='#4584b6'/>
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faJava} color='#f89820'/>
+                    </div>
+                    <div className='face7'>
+                        <FontAwesomeIcon icon={faGitAlt} color='#ffffff'/>
+                    </div>
+                    <div className='face8'>
+                        <FontAwesomeIcon icon={faGithub} color='#000000'/>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
+        <Loader type='pacman'/>
+        </>
     )
 }
 
