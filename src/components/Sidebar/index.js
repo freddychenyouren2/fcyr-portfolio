f752464aaf24211c76d9f4ad75da72c525c0fd9d
freddychenyouren2 <freddychenyouren2@gmail.com>
2024-02-20 02:16:44 +0800

    Reset changes again for routing issues
    KIV for routing issues
    
    For now access website only through homepage first, and may have fcyr-portfolio two times

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -13,16 +13,16 @@ const Sidebar = () => (
             <img className='sub-logo' src={LogoFSubtitle} alt="Freddy Chen You Ren" />
         </Link>
         <nav>
-            <NavLink exact="true" activeClassname="active" to="/">
+            <NavLink exact="true" activeClassname="active" to="/fcyr-portfolio">
                 <FontAwesomeIcon icon = {faHome} color='#4d4d4e'/>
             </NavLink>
-            <NavLink exact="true" activeClassname="active" className="about-link" to="/about">
+            <NavLink exact="true" activeClassname="active" className="about-link" to="/fcyr-portfolio/about">
                 <FontAwesomeIcon icon = {faUser} color='#4d4d4e'/>
             </NavLink>
-            <NavLink exact="true" activeClassname="active" className="contact-link" to="/contact">
+            <NavLink exact="true" activeClassname="active" className="contact-link" to="/fcyr-portfolio/contact">
                 <FontAwesomeIcon icon = {faEnvelope} color='#4d4d4e'/>
             </NavLink>
-            <NavLink exact="true" activeClassname="active" className="portfolio-link" to="/portfolio">
+            <NavLink exact="true" activeClassname="active" className="portfolio-link" to="/fcyr-portfolio/portfolio">
                 <FontAwesomeIcon icon = {faBarsProgress} color='#4d4d4e'/>
             </NavLink>
         </nav>
